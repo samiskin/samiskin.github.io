c880c1686e57a216d707e630c57354e1dc23abe6
Shiranka Miskin <shiranka.miskin@gmail.com>
2016-01-17 01:24:25 -0500

    made link direct to pdf

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from 'App.jsx';
 import Main from 'Main.jsx';
-import ResumeView from 'ResumeView.jsx';
 
 const reactHost = global.document.createElement('span');
 global.document.body.appendChild(reactHost);
@@ -11,7 +10,6 @@ global.document.body.appendChild(reactHost);
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="resume.pdf" component={ResumeView}/>
       <IndexRoute component={Main}/>
     </Route>
   </Router>
